a1bed306589f95566748735f17113c01c786b521
W4terF4ll <56806946+W4terF4ll@users.noreply.github.com>
2021-11-19 21:44:17 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,23 +3,32 @@ var upgrade = 0;
 var oneCost = 10;
 var oneCount = 0;
 var oneBought = false;
-var oneCostNext = 0;
+var oneCostNext = 11;
 
-function cookieClick(number){
-    cookies = cookies + number + upgrade;
+function cookieUpdate(number, upgradeCalc) {
+	if (upgradeCalc == true) {
+		cookies = cookies + number + upgrade;
+	} else {
+		cookies = cookies + number
+	}
 	document.getElementById("cookies").innerHTML = cookies;
 	if (cookies == 10 && oneBought == false) {
-		document.getElementById("one").innerHTML = "<button onclick=\"cookieAdd()\">Bake More! (<span id=\"oneCost\">10</span> Cookies)</button>";
+		document.getElementById("one").innerHTML = "<button id=\"oneButton\" onclick=\"cookieAdd()\">Bake More! (<span id=\"oneCost\">10</span> Cookies)</button>";
 		oneBought = true;
+	} else if (oneBought == true) {
+		if (cookies >= oneCostNext) {
+			oneButton.removeAttribute("disabled");
+		} else {
+			oneButton.setAttribute("disabled", "disabled");
+		}
 	}
 }
 
-function cookieAdd(){
+function cookieAdd() {
 	oneCost = Math.floor(10 * Math.pow(1.1,oneCount));
-	if (cookies >= oneCost){
+	if (cookies >= oneCost) {
 		upgrade += 1;
-		cookies -= oneCost;
-		document.getElementById("cookies").innerHTML = cookies;
+		cookieUpdate(-(oneCost), false);
 		oneCount += 1;
 		oneCostNext = Math.floor(10 * Math.pow(1.1,oneCount));
 		document.getElementById("oneCost").innerHTML = oneCostNext;
