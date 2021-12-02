fa906da2f0f469b683de741c0dc199d278eea011
W4terF4ll <56806946+W4terF4ll@users.noreply.github.com>
2021-12-02 04:31:00 -0500

    Update main.js

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ function bakeryAdd() {
 	bakeryBought = true;
 	document.getElementById("bakeryText").innerHTML = "<p class=\"bakeryText\">-- BAKERY --</p>"
 	document.getElementById("bakeryInfo").innerHTML = "<p class=\"bakeryInfo\">Click the lit up ovens to gain a stacking multiplier!</p>"
-	document.getElementById("bakeryMultiDisplay").innerHTML = "<p class=\"bakeryMultiText\">Current Bonus Multiplier: <span class=\"bakeryMultiTextStyle\" id=\"bakeryMultiText\">1</span>x</p>"
+	document.getElementById("bakeryMultiDisplay").innerHTML = "<p class=\"bakeryMultiText\">Current Bonus Multiplier: <span class=\"bakeryMultiTextStyle\" id=\"bakeryMultiText\">1</span><span class=\"bakeryMultiTextStyle\">x</span></p>"
 	document.getElementById("bakeryTopLeft").innerHTML = "<button id=\"bakeryManager1\" class=\"bakeryStyle\" onclick=\"bakeryUnlight(1)\"></button>";
 	document.getElementById("bakeryTopMid").innerHTML = "<button id=\"bakeryManager2\" class=\"bakeryStyle\" onclick=\"bakeryUnlight(2)\"></button>";
 	document.getElementById("bakeryTopRight").innerHTML = "<button id=\"bakeryManager3\" class=\"bakeryStyle\" onclick=\"bakeryUnlight(3)\"></button>";
