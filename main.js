efb18cc54bacc8e19b9e6193678022c0bcd44bf7
W4terF4ll <56806946+W4terF4ll@users.noreply.github.com>
2021-12-02 04:37:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ function bakeryAdd() {
 	cookieUpdate(-(bakeryCost), false);
 	bakeryBought = true;
 	document.getElementById("bakeryText").innerHTML = "<p class=\"bakeryText\">-- BAKERY --</p>"
-	document.getElementById("bakeryInfo").innerHTML = "<p class=\"bakeryInfo\">Click the lit up ovens to gain a stacking multiplier!</p>"
+	document.getElementById("bakeryInfo").innerHTML = "<p class=\"bakeryInfo\">Click lit up ovens for a bonus multiplier!</p>"
 	document.getElementById("bakeryMultiDisplay").innerHTML = "<p class=\"bakeryMultiText\">Current Bonus Multiplier: <span class=\"bakeryMultiTextStyle\" id=\"bakeryMultiText\">1</span><span class=\"bakeryMultiTextStyle\">x</span></p>"
 	document.getElementById("bakeryTopLeft").innerHTML = "<button id=\"bakeryManager1\" class=\"bakeryStyle\" onclick=\"bakeryUnlight(1)\"></button>";
 	document.getElementById("bakeryTopMid").innerHTML = "<button id=\"bakeryManager2\" class=\"bakeryStyle\" onclick=\"bakeryUnlight(2)\"></button>";
