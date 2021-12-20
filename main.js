19f51f82dc5d1ea248b1a29ac269b0b45b503c44
W4terF4ll <56806946+W4terF4ll@users.noreply.github.com>
2021-12-20 11:27:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -261,9 +261,12 @@ function mixerAdd() {
 }
 //mixer buttons (heavier wip)
 function mixerDetect(evt) {
+	alert(evt);
 	evt = (evt) ? evt : (window.event) ? event : null;
+	alert(evt);
   	if (evt) {
 		var charCode = (evt.charCode) ? evt.charCode :((evt.keyCode) ? evt.keyCode :((evt.which) ? evt.which : 0));
+		alert(charCode);
 		if (charCode == 37) {
 			alert("left arrow pressed");
 		} else if (charCode == 38) {
